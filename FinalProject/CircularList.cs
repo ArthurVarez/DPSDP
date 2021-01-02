e678df19a4e4ebf10b24461feca8ead10a833ff5
ClementMJ <c.muffatjoly@gmail.com>
2021-01-02 12:48:12 +0100

    circular list

--- FinalProject/CircularList.cs
+++ FinalProject/CircularList.cs
@@ -5,32 +5,64 @@ namespace FinalProject
 
     public class CircularList<T>
     {
-        public int value { get; set; }
+        public T value { get; set; }
         public Node<T> root { get; set; }
         public Node<T> tail { get; set; }
+        public int sizeofList;
 
 
         public CircularList()
         {
         }
-        public CircularList(int value, Node<T> root, Node<T> tail)
+        public CircularList(T value)
         {
             this.value = value;
-            this.root = root;
-            this.tail = tail;
+            this.root = new Node<T>(value);
+            var node = new Node<T>(value);
+            node.next = node;
+            this.root = node;
+            this.tail = node;
+            sizeofList = 1;
 
 
         }
         public void AddNode(Node<T> node)
         {
             Node<T> new_node = new Node<T>(node.data);
+            if (this.root == null)
+            {
+                new_node.next = this.root;
+                this.root = new_node;
+                sizeofList++;
 
-            if (root == null)
-                root = new_node;
+            }
+            new_node.next = this.root;
+            this.tail = new_node;
+            this.tail.next = this.root;
+            sizeofList++;
 
-            Node<T> temp = 
-            this.tail.next = new_node;
-            new_node.next = root;
+        }
+
+        public void DeleteNode(Node<T> node)
+        {
+            Node<T> temp = this.root;
+            if (node == this.root)
+            {
+                this.root = this.root.next;
+                this.tail.next = this.root;
+                sizeofList--;
+            }
+
+            for (int i = 0; i < sizeofList - 1; i++)
+            {
+                if(node == temp)
+                {
+                    temp = temp.next;
+                    sizeofList--;
+                }
+                temp = temp.next;
+
+            }
 
         }
 
