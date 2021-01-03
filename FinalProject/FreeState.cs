43e07130467170a20dc0f0e8a89946a49d687288
ClementMJ <c.muffatjoly@gmail.com>
2021-01-03 16:30:09 +0100

    state pattern

---
+++ FinalProject/FreeState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace FinalProject
+{
+    public class FreeState:State
+    {
+        private State state;
+
+        public FreeState(State state)
+        {
+            this.state = state;
+        }
+        public override void Play(Player player)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+            player.Index += die1.DieValue + die2.DieValue;
+            if (player.Index == 30)
+            {
+                //this.state = JailState;
+            }
+            else if (player.Index > 39)
+            {
+                player.Index = player.Index - 40;
+            }
+        }
+    }
+}
