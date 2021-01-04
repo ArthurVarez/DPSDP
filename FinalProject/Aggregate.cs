e4b55adce2e92e21ac53bcf374560cc7f962084e
ClementMJ <c.muffatjoly@gmail.com>
2021-01-04 17:45:28 +0100

    je test iterator

---
+++ FinalProject/Aggregate.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace FinalProject
+{
+    public interface Aggregate
+    {
+        Iterator createIterator();
+    }
+}
---
+++ FinalProject/CollectionIterator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FinalProject
+{
+    public class CollectionIterator
+    {
+        public CollectionIterator()
+        {
+        }
+    }
+}
